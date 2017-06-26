6b10c911f07c4f348b1b31d179888314efb02437
richardxin <richardxin168@yahoo.com>
2017-06-26 12:41:00 -0700

    update

--- src/main/java/hello_storm/TopologyStarter.java
+++ src/main/java/hello_storm/TopologyStarter.java
@@ -8,11 +8,11 @@ public class TopologyStarter {
 
 	public static void main(String[] args) {
 		TopologyBuilder tb = new TopologyBuilder();
-		tb.setSpout("my_spout_id", new MySpout());
-		tb.setBolt("my_bolt_id", new MyBolt()).shuffleGrouping("my_spout_id");
+		tb.setSpout("spout_id", new MySpout());
+		tb.setBolt("bolt_id", new MyBolt()).shuffleGrouping("spout_id");
 
 		Config conf = new Config();
 		LocalCluster lc = new LocalCluster();
-		lc.submitTopology("Hello Storm", conf, tb.createTopology());
+		lc.submitTopology("Hello_Storm", conf, tb.createTopology());
 	}
 }
